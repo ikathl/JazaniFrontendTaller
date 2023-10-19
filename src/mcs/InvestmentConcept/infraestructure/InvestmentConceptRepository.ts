8ad8e6d4ab939d3562df6903eee57cc6b8fb9efe
Ines Katia <150395y@gmail.com>
2023-10-18 22:30:33 -0500

    redireccionamiento con navigate

--- src/mcs/InvestmentConcept/infraestructure/InvestmentConceptRepository.ts
+++ src/mcs/InvestmentConcept/infraestructure/InvestmentConceptRepository.ts
@@ -5,7 +5,7 @@ import { type InvestmentConceptResponse } from '../domain';
 export const findAll = async (): Promise<InvestmentConceptResponse[]> => {
 	const response: AxiosResponse<InvestmentConceptResponse[]> = await axios.get<
 		InvestmentConceptResponse[]
-	>(`https://localhost:7044/api/investmentconcept `);
+	>(`${API_BASE_URL}/api/investmentconcept`);
 
 	return response.data;
 };
